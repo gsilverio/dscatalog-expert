02b8de9674f6635612b5c108c04496a55cff9274
Guilherme <guilhermegsilverio@gmail.com>
2023-08-09 17:13:50 -0300

    Category find by id, exception handler

--- dscatalog/backend/dscatalog/src/main/java/com/dscatalogGSilverio/dscatalog/resources/CategoryResource.java
+++ dscatalog/backend/dscatalog/src/main/java/com/dscatalogGSilverio/dscatalog/resources/CategoryResource.java
@@ -1,11 +1,14 @@
 package com.dscatalogGSilverio.dscatalog.resources;
 /* Resource = Controllers */
 
+import com.dscatalogGSilverio.dscatalog.dto.CategoryDTO;
 import com.dscatalogGSilverio.dscatalog.entities.Category;
 import com.dscatalogGSilverio.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,13 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+    @GetMapping(value="{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
---
+++ dscatalog/backend/dscatalog/src/main/java/com/dscatalogGSilverio/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.dscatalogGSilverio.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+    public static final long serialVersionUID=1L;
+
+    public EntityNotFoundException(String msg){
+        super(msg);
+    }
+}
