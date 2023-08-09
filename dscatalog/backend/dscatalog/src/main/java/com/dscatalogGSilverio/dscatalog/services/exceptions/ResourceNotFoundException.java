6cb88c26c328788524dce2db860bbbde235fa17d
Guilherme <guilhermegsilverio@gmail.com>
2023-08-09 18:54:23 -0300

    Category Post

--- dscatalog/backend/dscatalog/src/main/java/com/dscatalogGSilverio/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ dscatalog/backend/dscatalog/src/main/java/com/dscatalogGSilverio/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,9 @@
 package com.dscatalogGSilverio.dscatalog.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException{
     public static final long serialVersionUID=1L;
 
-    public EntityNotFoundException(String msg){
+    public ResourceNotFoundException(String msg){
         super(msg);
     }
 }
