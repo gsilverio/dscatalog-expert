3498e0ad4df00f43de24427014da0b7f532788be
Guilherme <guilhermegsilverio@gmail.com>
2023-08-09 22:17:28 -0300

    Product Entity

--- dscatalog/backend/dscatalog/src/main/java/com/dscatalogGSilverio/dscatalog/resources/CategoryResource.java
+++ dscatalog/backend/dscatalog/src/main/java/com/dscatalogGSilverio/dscatalog/resources/CategoryResource.java
@@ -6,6 +6,9 @@ import com.dscatalogGSilverio.dscatalog.entities.Category;
 import com.dscatalogGSilverio.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,8 +23,16 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value="page", defaultValue = "0") Integer page,
+            @RequestParam(value="linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value="direction", defaultValue = "ASC") String direction,
+            @RequestParam(value="orderBy", defaultValue = "name") String orderBy
+
+    ){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
+
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
     @GetMapping(value="{id}")
